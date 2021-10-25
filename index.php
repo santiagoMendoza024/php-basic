7ec77cde66ddbdbe74790b184d8be854a2bb5d5f
Santiago Mendoza <escoem@outlook.com>
2021-10-25 11:31:31 -0500

    Basic

---
+++ index.php
@@ -0,0 +1,22 @@
+<!--Para trabajar con php en un solo archivo basta abrir la etiqueta de php-->
+<?php
+
+class Answer
+{
+    //Comentario de una sola línea
+    # Comentario 
+    /*
+        Comentario de varias líneas
+    */
+    /**
+     * Proporciona diseño
+     * Varias líneas
+     * Otra palabra
+     */
+    protected $clientes = [];
+    /**
+     * Organizar nuestro código
+     * Código comentado
+     */
+    protected $insurers = [];
+}
---
+++ index1.php
@@ -0,0 +1,17 @@
+<!--Todo comienza con la etiqueta de apertura php-->
+<?php
+    print 'Prueba desde etiqueta php';
+?>
+<!--Tambien tenemos la posibilidad de escribir estructura o bloque de código html-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primera Prueba</title>
+</head>
+<body>
+    <h1>Prueba HTML</h1>
+</body>
+</html>
