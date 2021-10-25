09fc61b413296bb3c729908b15812be8d084e447
Santiago Mendoza <escoem@outlook.com>
2021-10-25 11:42:28 -0500

    asignación y aritmética

---
+++ indexV7.php
@@ -0,0 +1,23 @@
+<?php
+
+// Asignación
+//Asignamos simplemente con el '='
+$num = 8;
+$lang = [
+    // => tambien se usa para asignación
+    'es' => 'Español',
+    'en' => 'Inglés'
+];
+// Aritmética
+//El (int) es para especificar que vamos a sumar numeros
+echo "Suma 2 + 2 = ". ((int) 2 + (int)2);
+echo '<br>';
+echo "Resta 2 - 2 = ". ((int) 2 - (int)2);
+echo '<br>';
+echo "Multiplica 2 * 2 = ".  2 * 2;
+echo '<br>';
+echo "División 2 / 2 = ".  2 / 2;
+echo '<br>';
+echo "Módulo 2 % 2 = ".  2 % 2;
+echo '<br>';
+echo "Exponencial 2 ** 2 = ".  2 ** 2;
